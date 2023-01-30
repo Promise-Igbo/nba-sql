bbd6661b9a8a8625c60425fe5a236623e5a3ec1b
Promise Igbo <100773978+Promise-Igbo@users.noreply.github.com>
2023-01-30 14:20:49 +0100

    Updated

--- scripts/create_mysql.sh
+++ scripts/create_mysql.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-DB_NAME="nba" DB_HOST="localhost" DB_USER=nba_sql DB_PASSWORD=nba_sql python stats/nba_sql.py --default-mode --database="mysql" --create-schema
+DB_NAME="nba" DB_HOST="testserver09.mysql.database.azure.com" DB_USER=promise DB_PASSWORD=password@123 python stats/nba_sql.py --default-mode --database="mysql" --create-schema
